6cd7ade9b8101fdc1b1791f6cb0bf5ae4e4d1f2a
Guillermo <ggarciaperez@danielcastelao.org>
2021-01-27 11:16:20 +0100

    Code cleaning and usefull comments added

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -6,8 +6,9 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 1;
     // because speed is public i can see it on the unity menu
+    public float speed = 1;
+    // To display and associate the "UI" 
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
     public GameObject looseTextObject;
@@ -19,26 +20,10 @@ public class PlayerController : MonoBehaviour
     private float movementY;
     private Vector3 startPos;
 
-    //added to try and use accelerometer
     void Main()
     {
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
     }
-    /*
-    void Update()
-    {
-        if (SystemInfo.deviceType == DeviceType.Desktop)
-        {
-            if (Input.GetKey("escape"))
-                Application.Quit();
-        }
-        else
-        {
-            if (Input.GetKeyDown(KeyCode.Escape))
-                Application.Quit();
-        }
-    }
-    */
 
     // Start is called before the first frame update
     void Start()
@@ -46,10 +31,12 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         count = 0;
         SetCountText();
+        //Hiding Win/Loose text and Reset Button.
         winTextObject.SetActive(false);
         looseTextObject.SetActive(false);
         restartButton.SetActive(false);
-        startPos = this.transform.position;
+        //Player start position.
+        startPos = this.transform.position;        
     }
 
     void OnMove(InputValue movementValue)
@@ -72,41 +59,23 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        /*
-        if (rb = null)
-        {
-            Debug.Log("Failed to get rigidbody component from gameobject.");
-        }
-        else
-            Debug.Log("Rigidbody component from gameobject is aquired.");
-        */
-        //Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        //rb.AddForce(movement * speed);
+        //this if call was made to use same script for accelerometer or keyboard but aint working, will leave it there anyways
         if (SystemInfo.deviceType == DeviceType.Desktop)
         {
             Vector3 movement = new Vector3(movementX, 0.0f, movementY);
             rb.AddForce(movement * speed);
         }
         else
-        {
+        {   
             Vector3 movement = new Vector3(Input.acceleration.x, 0.0f, Input.acceleration.y);
-            /*
-            //Vector3 acclerometerValue = rawAccelValue();
-            //Debug.Log("X: " + acclerometerValue.x + "Y: " + acclerometerValue.y + " Z: " + acclerometerValue.z);
-            //rb.AddForce(acclerometerValue * speed);
-            */
             rb.AddForce(movement * speed);
         }
 
     }
-    /*
-    Vector3 rawAccelValue()
-    {
-        return Input.acceleration;
-    }
-    */
+
     private void OnTriggerEnter(Collider other)
     {
+        // Player picking up good items
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
@@ -114,12 +83,14 @@ public class PlayerController : MonoBehaviour
             //each time u hit a ball the count goes up by 1
             SetCountText();
         }
+        //Enemy hitting the player
         if (other.gameObject.CompareTag("Enemy")){
+            //Player goes to start position and looses points.
             this.transform.position=startPos;
             count = count -1;
             if (count<0){
+                //When player is out of points cant move and a Reset button appears on screen.
                 looseTextObject.SetActive(true);
-                //easy way to stop the game while i work on a reset button
                 speed=0;
                 movementX=0;
                 movementY=0;
